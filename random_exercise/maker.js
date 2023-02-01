0e0533f3654f24128162bbf0780e00c8f8d22d11
dredegui <guilherme.m.pascoal@tecnico.ulisboa.pt>
2023-02-01 11:57:02 +0000

    Fix code org

--- random_exercise/maker.js
+++ random_exercise/maker.js
@@ -1,15 +1,18 @@
 const bd = document.querySelector("body");
 
 const para1 = document.createElement('p');
+
 const para2 = document.createElement('p');
 
 const header1 = document.createElement('h1');
+
 const header3 = document.createElement('h3');
 
 const div = document.createElement('div');
 
 para1.textContent = "Hey I'm red!";
 para1.style["color"] = "red";
+
 para2.textContent = "ME TOO";
 
 header1.textContent = "I'm in a div";
